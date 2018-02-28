c0cc0121145cf04aec073c67b383f58beeb8bf76
WcmPersonalRepository <723385077@qq.com>
2018-02-28 17:04:07 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+   <Mheader></Mheader>
+    <Tab></Tab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import Mheader from './components/m-header/m-header'
+import Tab from './components/tab/tab'
+import router from './router/index.js'
+
+export default {
+  router,
+  components: {
+    Mheader,
+    Tab
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
